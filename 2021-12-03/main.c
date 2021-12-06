e025b7d4a9a971a2fac3ac2add02f10079ef848e
Joel Brewer <joel@brewerdigital.com>
2021-12-05 22:18:13 -0700

    Finish AOC Day 3 Part 1

--- 2021-12-03/main.c
+++ 2021-12-03/main.c
@@ -3,26 +3,28 @@
 #include <math.h>
 #include <string.h>
 #include "../lib/jlib.h"
-#define NUMBITS 5
+#define NUMBITS 12
 
 int day3part1();
+int day3part2();
 int fromBinary(const char * str);
 
 int main()
 {
   printf("Day 3 Part 1 : %d\n", day3part1());
 }
-// create an array of ints to hold the number of on-bits for each position
-// eg: [1,2,3,4,5,6,7,8,9,10,11,12]
 
 int day3part1()
 {
   int counts[NUMBITS] = {0,0,0,0,0};
-  int output[NUMBITS] = {0,0,0,0,0};
+  char output[NUMBITS] = {'0','0','0','0','0'};
   int totalLines = 0;
-  char const* const fileName = "test3.txt";
+  char const* const fileName = "input3.txt";
   FILE* file = fopen(fileName, "r");
   char line[512];
+
+  int gamma = 0;
+  int epsilon = 0;
 
   while (fgets(line, sizeof(line), file)) {
     int i = 0;
@@ -30,23 +32,23 @@ int day3part1()
     totalLines++;
 
     while ((c = line[i]) != '\n') {
-      printf("c : %d\n", c - '0');
       counts[i] += c - '0';
       i++;
     }
   }
-  printf("Total lines : %d\n", totalLines);
+
   for (int i = 0; i < NUMBITS; i++) {
     if (counts[i] > totalLines/2) {
-      output[i] = 1;
+      output[i] = '1';
     }
-    //printf("counts[%d] : %d\n", i, counts[i]);
-    printf("output[%d] : %d\n", i, output[i]);
   }
-  printf("output as decimal %d\n", fromBinary(output));
-  return 0;
-}
 
-int fromBinary(const char *s) {
-  return (int) strtol(s, NULL, 2);
+	for (int i = 0; i < NUMBITS ; i++) {
+		if (output[NUMBITS-i-1] - '0' == 1) {
+			gamma += pow(2,i);
+		} else {
+			epsilon += pow(2,i);
+		}
+	}
+  return gamma*epsilon;
 }
