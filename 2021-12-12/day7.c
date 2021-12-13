8c5c57fe6c035c58a1c740ceaa0186b34c7a8cd1
Joel Brewer <joel@brewerdigital.com>
2021-12-12 18:35:15 -0700

    Prep Day 7

---
+++ 2021-12-12/day7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include "../lib/jlib.h"
+
+void part1()
+{
+}
+
+void part2()
+{
+}
+
+int main()
+{
+  part1();
+  part2();
+}
