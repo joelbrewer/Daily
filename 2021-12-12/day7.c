8aa3289e44291804ff6c0395363cee77e1f9f5f6
Joel Brewer <joel@brewerdigital.com>
2021-12-12 18:40:03 -0700

    Start on Day 7

--- 2021-12-12/day7.c
+++ 2021-12-12/day7.c
@@ -3,6 +3,40 @@
 #include <math.h>
 #include <string.h>
 #include "../lib/jlib.h"
+#define MAXALIGN 16
+
+struct node {
+  int data;
+  struct node *next;
+};
+
+struct node *head = NULL;
+void insertFirst(int data)
+{
+  struct node *link = (struct node*) malloc(sizeof(struct node));
+  link->data = data;
+  link->next = head;
+  head = link;
+}
+
+void parsePositions()
+{
+  char const* const fileName = "test7.txt";
+  char input[5000];
+  const char delimiters[] = ",";
+  FILE* file = fopen(fileName, "r");
+  char *num;
+  fscanf(file, "%[^\n]", input);
+  num = strtok(input,delimiters);
+  while(num != NULL) {
+    insertFirst(atoi(num));
+    num = strtok(NULL, delimiters);
+  }
+}
+
+void calculateFuel()
+{
+}
 
 void part1()
 {
