3e0bf7f638a2e640353f0885ee03808500f810cc
Joel Brewer <joel@brewerdigital.com>
2022-02-13 15:04:16 -0700

    K&R 2-6

---
+++ 2022-02-13/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+/* getbits: get n bits from position p */
+unsigned getbits(unsigned x, int p, int n)
+{
+  return (x >> (p+1-n)) & ~(~0 << n);
+}
+
+/* decToBinary: print binary representation of int n */
+void decToBinary(int n)
+{
+  printf("\n");
+  for(int i = 31; i >= 0; i--) {
+    int k = n >> i;
+    if (k & 1)
+      printf("1");
+    else
+      printf("0");
+  }
+}
+
+unsigned setbits(unsigned x, int p, int n, unsigned y)
+{
+  return ((x & ~0<<n) | getbits(y, p, n));
+}
+
+int main() {
+  printf("Hello World\n");
+  unsigned int a = 60; /* 0011 1100 */
+  unsigned int b = 13; /* 0000 1101 */
+  int c = 0;
+
+  c = a & b;           /* 0000 1100 = 12  */
+  c = a | b;           /* 0011 1101 = 61  */
+  c = a ^ b;           /* 0011 0001 = 49  */
+  c = a << 2;          /* 1111 0000 = 240 */
+  c = a >> 2;          /* 0000 1111 = 15  */
+  c = ~a;              /* 1100 0011 =  */
+
+  unsigned x = 170; // 10101010
+  unsigned y = 56;  // 00111000
+
+  printf("%d\n", getbits(b, 3, 4));
+  decToBinary(getbits(b, 3, 4));
+  decToBinary(~0);
+  printf("\nx:");
+  decToBinary(x);
+  printf("\ny:");
+  decToBinary(y);
+  decToBinary(setbits(x, 3, 4, y));
+};
